5f37fb1b0f7384b509e35cbf1483bf6dfb038663
120864 <Michael.Schreiber+sncorp@sncorp.com>
2025-06-27 13:12:10 -0700

    created book_info class

--- book_info.py
+++ book_info.py
@@ -1,1 +1,9 @@
-class book(title, author, pages, )
+class book:
+    def __init__(self, title, firstName, lastName, publicationDate, pageCount):
+        self.title: title
+        self.authorFName: firstName
+        self.authorLName: lastName
+        self.publicationDate: publicationDate
+        self.pageCount: pageCount
+
+    def newBook(self):
