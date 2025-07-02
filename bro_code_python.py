a22d18ac5ba697c62dabd1eb603ca72a9e2501a4
120864 <Michael.Schreiber+sncorp@sncorp.com>
2025-07-02 14:17:45 -0700

    added some stuff

---
+++ bro_code_python.py
@@ -0,0 +1,21 @@
+import math
+
+age = 25
+num_of_students = 33
+
+print(f"{num_of_students} is the number of kids in the class")
+
+print (age)
+age += 3
+
+print(age)
+print(age)
+print(age + num_of_students)
+
+print
+
+x = 3.14
+y = 2
+z = math.pi
+
+print(z)
